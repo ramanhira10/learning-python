529e5d38ca0502bcadad377b1b4e0d926d8b56ce
Raman Hira <ramanhira@gmail.com>
2024-08-12 22:25:31 +0530

    updated for exercise for 2 day

---
+++ day-2/exercise-1.py
@@ -0,0 +1,5 @@
+# A program to demonstrate the use of nested loops - for loop
+for i in range(5, 0, -1):
+    for j in range (1, i + 1, 1):
+        print (j, end="");
+    print();
---
+++ day-2/exercise-2.py
@@ -0,0 +1,4 @@
+for i in range (12, 19, 2):
+    for j in range(18, i - 1, -2):
+        print(j, end=" ");
+    print();
