cc28d2b61eda1c03b064a5e701a7774ef07b382a
Raman Hira <ramanhira@gmail.com>
2024-08-12 22:19:15 +0530

    updated the files day wise

---
+++ day-1/abc3.py
@@ -0,0 +1,7 @@
+# A program to demonstrate the use of or operator
+ch = 'i';
+
+if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'u'):
+    print(ch, 'is vowel');
+else:
+    print(ch, 'is not vowel');
